b1793191fdb6a5e2f620f0d74a109503be1fdd7a
Mike Hamer <mhamer@bandwidth.com>
2021-12-10 11:10:17 -0500

    Remove remaining references to log4j

--- src/test/java/com/bandwidth/iris/sdk/BaseModelTests.java
+++ src/test/java/com/bandwidth/iris/sdk/BaseModelTests.java
@@ -1,12 +1,6 @@
 package com.bandwidth.iris.sdk;
 
 import com.github.tomakehurst.wiremock.junit.WireMockRule;
-import org.apache.commons.configuration.ConfigurationException;
-import org.apache.log4j.ConsoleAppender;
-import org.apache.log4j.Level;
-import org.apache.log4j.Logger;
-import org.apache.log4j.PatternLayout;
-import org.junit.Before;
 import org.junit.Rule;
 import org.junit.rules.ExpectedException;
 
@@ -20,15 +14,6 @@ public class BaseModelTests {
 
     protected IrisClient getDefaultClient() {
         return new IrisClient("http://localhost:8090", "accountId", "username", "password", "v1.0");
-    }
-
-    @Before
-    public void setUp() throws ConfigurationException {
-        org.apache.log4j.BasicConfigurator.configure();
-        Logger.getRootLogger().setLevel(Level.DEBUG);
-        ConsoleAppender a = (ConsoleAppender) Logger.getRootLogger().getAllAppenders().nextElement();
-        a.setLayout(new PatternLayout("%d{ABSOLUTE} %5p %c{1}:%L - [%t] %m%n"));
-
     }
 
     public void setMessage(String s) {
