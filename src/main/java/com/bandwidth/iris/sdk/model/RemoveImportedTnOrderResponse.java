83b638a30898662316d54d6b381c2ba3473ae865
AJ Rice <53190766+ajrice6713@users.noreply.github.com>
2022-09-19 12:54:36 -0400

    Fix XML Root Element Name

--- src/main/java/com/bandwidth/iris/sdk/model/RemoveImportedTnOrderResponse.java
+++ src/main/java/com/bandwidth/iris/sdk/model/RemoveImportedTnOrderResponse.java
@@ -5,7 +5,7 @@ import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
-@XmlRootElement(name="RemoveImportedTnOrderResponse")
+@XmlRootElement(name="RemoveImportedTnOrdersResponse")
 @XmlAccessorType(XmlAccessType.FIELD)
 public class RemoveImportedTnOrderResponse {
 
