bb9ea8806d3ea43fadc280feafd15b880a99e738
Brian <bgome004@gmail.com>
2022-12-07 07:38:55 -0500

    DX-3061

--- src/main/java/com/bandwidth/iris/sdk/model/ImportTnCheckerPayload.java
+++ src/main/java/com/bandwidth/iris/sdk/model/ImportTnCheckerPayload.java
@@ -8,6 +8,13 @@ import java.util.List;
 @XmlAccessorType(XmlAccessType.FIELD)
 public class ImportTnCheckerPayload extends BaseModel {
 
+    @XmlElement( name = "SiteId")
+    private int siteId;
+
+    @XmlElement( name = "SipPeerId")
+    private int sipPeer;
+
+
     @XmlElementWrapper(name = "TelephoneNumbers")
     @XmlElement(name = "TelephoneNumber")
     private List<String> telephoneNumberList;
@@ -29,5 +36,21 @@ public class ImportTnCheckerPayload extends BaseModel {
 
     public void setImportTnErrorList(List<ImportTnError> importTnErrorList) {
         this.importTnErrorList = importTnErrorList;
+    }
+
+    public int getSiteId() {
+        return siteId != 0 ? siteId : null;
+    }
+
+    public void setSiteId(int siteId) {
+        this.siteId = siteId;
+    }
+
+    public int getSipPeer() {
+        return sipPeer != 0 ? sipPeer : null;
+    }
+
+    public void setSipPeer(int sipPeer) {
+        this.sipPeer = sipPeer;
     }
 }
