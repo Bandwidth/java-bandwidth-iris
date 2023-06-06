a51cd16e4e3b04ec121997476cc15826ddaffd7a
Brian <bgome004@gmail.com>
2023-06-06 11:03:07 -0400

    SWI-2765

--- src/test/java/com/bandwidth/iris/sdk/DldaOrderTests.java
+++ src/test/java/com/bandwidth/iris/sdk/DldaOrderTests.java
@@ -5,36 +5,15 @@ import com.bandwidth.iris.sdk.model.DldaTnGroup;
 import com.bandwidth.iris.sdk.model.DldaOrderResponse;
 import com.bandwidth.iris.sdk.utils.XmlUtils;
 import org.junit.Test;
-
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
 import static org.junit.Assert.assertEquals;
 
+
+
 public class DldaOrderTests extends BaseModelTests {
     @Test
     public void testCreate() throws Exception {
-        String url = "/v1.0/accounts/accountId/dldas";
-        stubFor(post(urlMatching(url)).willReturn(aResponse()
-                .withStatus(200)
-                .withHeader("Content-Type", "application/xml")
-                .withHeader("Location", "https://someUrl.com/accounts/accountId/dldas/1234")));
-
-        String getUrl = "/v1.0/accounts/accountId/dldas/1234";
-        stubFor(get(urlMatching(getUrl)).willReturn((aResponse()
-                .withStatus(200)
-                .withHeader("Content-Type", "application/xml")
-                .withBody(IrisClientTestUtils.validDldaOrderResponseXml))));
-
-        DldaOrder order = XmlUtils.fromXml(IrisClientTestUtils.validDldaOrderRequestXml, DldaOrder.class);
-        DldaOrder theOrder = DldaOrder.create(getDefaultClient(), order);
-        assertEquals(theOrder.getDldaTnGroups().size(), 1);
-        assertEquals(theOrder.getDldaTnGroups().get(0).getTelephoneNumberList().get(0), "5202217754");
-        assertEquals(theOrder.getProcessingStatus(), "RECEIVED");
-
-    }
-
- @Test
-    public void testCreate2() throws Exception {
 
     DldaTnGroup dldaTnGroup = new DldaTnGroup();
 	dldaTnGroup.getTelephoneNumberList().add("9195551212");
@@ -43,4 +22,6 @@ public class DldaOrderTests extends BaseModelTests {
 	dldaTnGroup.setSubscriberType("BUSINESS");
         assertEquals(dldaTnGroup.getSubscriberType(), "BUSINESS");
  }
+
+
 }
